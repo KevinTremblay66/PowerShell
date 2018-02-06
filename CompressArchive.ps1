b879230b679c2676a3321f6fc393815df0f36d92
Kevin Tremblay <kevin@firebrandtech.com>
2018-02-06 10:14:28 -0500

    Testing adding via command line to my github

---
+++ CompressArchive.ps1
@@ -0,0 +1,24 @@
+﻿##############################
+#
+#   COMPRESS ARCHIVE
+#
+#   CREATED : April 2017
+#
+##############################
+
+$source1 = "C:\_Projects"
+$source2 = "C:\_Documents"
+$source3 = "C:\_Development"
+
+$destination1 = "F:\MyPC\Projects.zip"
+$destination2 = "F:\MyPC\Documents.zip"
+$destination3 = "F:\MyPC\Development.zip"
+
+#Source1
+compress-archive -path $source1 -DestinationPath $destination1 -CompressionLevel Optimal -Force
+
+#Source2
+compress-archive -path $source2 -DestinationPath $destination2 -CompressionLevel Optimal -Force
+
+#Source3
+compress-archive -path $source3 -DestinationPath $destination3 -CompressionLevel Optimal -Force
